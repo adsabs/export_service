fbcd37b17fd33c12f0f380c63e43715040a9a517
Vladimir Sudilovsky <vsudilovsky@gmail.com>
2015-05-27 19:35:49 +0000

    wsgi: remove DispatcherMiddleware

--- wsgi.py
+++ wsgi.py
@@ -7,14 +7,9 @@
 """
 
 from werkzeug.serving import run_simple
-from werkzeug.wsgi import DispatcherMiddleware
 import app
 
-#from sample_application2 import app2 as sample_application2
-
-application = DispatcherMiddleware(app.create_app(),mounts={
-  #'/mount1': sample_application2.create_app(), #Could have multiple API-applications at different mount points
-  })
+application = app.create_app()
 
 if __name__ == "__main__":
     run_simple('0.0.0.0', 4000, application, use_reloader=False, use_debugger=True)
