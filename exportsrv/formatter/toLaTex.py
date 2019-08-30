8530074e72a980f89e107090411c2dfba5fe991a
golnazads <28757512+golnazads@users.noreply.github.com>
2019-08-30 13:12:08 -0400

    make sure $ is part of inline math mode before breaking the string to ignore the math mode chunck, and it is actually not dollar sign representation

--- exportsrv/formatter/toLaTex.py
+++ exportsrv/formatter/toLaTex.py
@@ -31,7 +31,15 @@ def encode_laTex(text):
     :return:
     """
     if (len(text) > 1):
-        chunks = re.split('(\$)', text)
+        # make we want to break on $...$ (In-line math) and not apply latex substitution there
+        # however, it could be dollar sign as in the following record's title
+        # "bibcode":"1979AstQ....3..143M",
+        # "title":["The Gemini Syndrome: Star Wars of the Oldest Kind. Roger Culver and Philip Ianna The Astronomy
+        # Quarterly Library Volume 1 Pachart Publishing House $11.95"],
+        if text.count('$') % 2 == 0:
+            chunks = re.split('(\$)', text)
+        else:
+            chunks = [text]
         latex = []
         i = 0
         while i < len(chunks):
